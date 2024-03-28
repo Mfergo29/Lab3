c4a600855e84519f3ca2de03ce982d13aa992962
Mfergo29 <mnfernandez@miuandes.cl>
2024-03-28 16:05:30 -0300

    Lab3 Final

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    def home
+        puts("HOLAS")
+    end
+    def about
+    end
+    def contact
+    end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController <ApplicationController
+    def home
+        puts("HOLAS")
+    end
+
+    def about
+    end
+    def contact
+    end
+    
+end
